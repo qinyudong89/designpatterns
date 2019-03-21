82b045071b1723f43242851370b32e6564d5dccc
qinyudong <qinyudong89@gemail.com>
2019-03-21 22:47:41 +0800

    更改启动类的名称

--- src/main/java/com/example/designpatterns/Main.java
+++ src/main/java/com/example/designpatterns/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesignpatternsApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesignpatternsApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
