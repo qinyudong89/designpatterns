a37942a4ec209a160229f83589cb32cc36b5cf0c
qinyudong <qinyudong89@gemail.com>
2019-03-23 11:18:02 +0800

    单例的四种实现方式

--- src/main/java/com/example/designpatterns/singleton/EnumSingleton.java
+++ src/main/java/com/example/designpatterns/singleton/EnumSingleton.java
@@ -3,8 +3,11 @@ package com.example.designpatterns.singleton;
 /**
  * @author yu dong qin
  * @ClassName:枚举实现单例（最简洁的方式）
- * @Description: 其功能完整、使用简洁、无偿地提供了序列化机制、在面对复杂的序列化或者反射攻击时仍然可以绝对防止多次实例化等优点，
+ * @Description: 线程安全代码还简洁、无偿地提供了序列化机制、在面对复杂的序列化或者反射攻击时仍然可以绝对防止多次实例化等优点，
  * 单元素的枚举类型被认为是实现Singleton的最佳方法。
+ * 枚举线程安全是因为，当一个Java类第一次被真正使用到的时候静态资源被初始化、Java类的加载和初始化过程都是线程安全的
+ * （因为虚拟机在加载枚举的类的时候，会使用ClassLoader的loadClass方法，
+ * 而这个方法使用同步代码块保证了线程安全）。所以，创建一个enum类型是线程安全的
  * @date
  */
 public enum EnumSingleton {
--- src/main/java/com/example/designpatterns/singleton/LazySingleton.java
+++ src/main/java/com/example/designpatterns/singleton/LazySingleton.java
@@ -3,25 +3,25 @@ package com.example.designpatterns.singleton;
 /**
  * @author yu dong qin
  * @ClassName:饿汉式单例
- * @Description:
- * 1、优点：实例在需要时才创建，节省内存
- * 2、缺点：如不经处理，会导致线程不安全、性能低
+ * @Description: 1、优点：实例在需要时才创建，节省内存
+ * 2、缺点：如不经处理，会导致线程不安全、性能低；无法解决反序列化会破坏单例的问题
  * 3、volatile的作用：多线程下保证变量的可见性、禁止语义重排序
  * 4：禁止语言重排序的作用：在某个线程创建单例对象时，在构造函数被调用前，
  * 就为该对象分配了内存空间并将对象的字段设置为默认值。此时就可以将分配的内存地址赋值
  * 给instance字段了，然而该对象可能还没初始化。若紧接着另一个线程来调用getInstance，
  * 获取到的就是状态不正确的对象，程序出错。
- *      而volatile的一个语义是禁止指令重排序优化，也就保证了instance变量被赋值的时候
+ * 而volatile的一个语义是禁止指令重排序优化，也就保证了instance变量被赋值的时候
  * 对象已经是初始化过的，从而避免了上述问题。
  * 5、双重检查避免，实例不唯一
  * @date
  */
 public class LazySingleton {
-    private volatile static LazySingleton INSTANCE ;
-    public static LazySingleton getInstance(){
-        if (INSTANCE == null){
-            synchronized(LazySingleton.class){
-                if (INSTANCE == null){
+    private volatile static LazySingleton INSTANCE;
+
+    public static LazySingleton getInstance() {
+        if (INSTANCE == null) {
+            synchronized (LazySingleton.class) {
+                if (INSTANCE == null) {
                     INSTANCE = new LazySingleton();
                 }
             }
@@ -29,7 +29,14 @@ public class LazySingleton {
         return INSTANCE;
     }
 
+    //防止单例在反序列化时被破坏
+    private Object readResolve() {
+        return INSTANCE;
+    }
+
     //禁止外部创建实例
-    private LazySingleton(){}
+    private LazySingleton() {
+    }
+
 
 }
