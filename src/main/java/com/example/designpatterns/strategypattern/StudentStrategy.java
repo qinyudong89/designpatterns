8d553705948ba9a2eeba56a65a7f2db219f8ae19
qinyudong <yudong.qin@topideal.com.cn>
2019-07-17 09:49:30 +0800

    代码优化

--- src/main/java/com/example/designpatterns/strategypattern/StudentStrategy.java
+++ src/main/java/com/example/designpatterns/strategypattern/StudentStrategy.java
@@ -11,6 +11,6 @@ public class StudentStrategy implements DiscountStrategy {
     private final static Double DISCOUNT = 0.5;
     @Override
     public void calculate(double price) {
-        System.out.println("学生票价为：" + (DISCOUNT*price));
+        System.out.println("学生票折后价为：" + (DISCOUNT*price));
     }
 }
